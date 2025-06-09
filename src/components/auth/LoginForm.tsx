a78a3a9e873cc64b89bcc070c667196cabcc4e2e
Eduardo Fiorini <edupva@gmail.com>
2025-06-08 23:50:12 -0400

    Updated LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -58,9 +58,9 @@ const LoginForm: React.FC = () => {
             transition={{ duration: 0.8 }}
           >
             <div className="flex items-center mb-8">
-              <div className="w-24 h-24 mr-4 flex items-center justify-center">
+              <div className="w-32 h-32 mr-4 flex items-center justify-center">
                 <img 
-                  src="/logo.png" 
+                  src="/logo-white.png" 
                   alt="Meu NPS" 
                   className="w-full h-full object-contain"
                 />
