30e68ff93c315739ea4c78bf6b168f54bbc26936
Eduardo Fiorini <edupva@gmail.com>
2025-07-11 17:38:13 -0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,4 +7,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)s
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
