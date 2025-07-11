67ee25b5910a3a44d6d8610ba52fe67d4ca3e2c2
Eduardo Fiorini <edupva@gmail.com>
2025-07-11 17:45:20 -0400

    Updated RegisterForm.tsx

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import Button from '../ui/Button';
 import Input from '../ui/Input';
+import { supabase } from '../../lib/supabase'
 import { 
   Check, 
   Users, 
